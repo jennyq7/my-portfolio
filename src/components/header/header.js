917afe7b2601e0e18cc9e179934fb485de2f28e6
codingisfun <61808280+jennyq7@users.noreply.github.com>
2024-01-04 22:09:03 -0500

    started work on header

---
+++ src/components/header/header.js
@@ -0,0 +1,15 @@
+
+function Header () {
+    return (
+        <header className="bg-primary">
+            <section>Jenny Quan</section>
+            <ul>
+                <li>About</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </header>
+    )
+};
+
+export default Header;
