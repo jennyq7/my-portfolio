091117e03ac9756dbebe2f819af9679dcd09786e
codingisfun <61808280+jennyq7@users.noreply.github.com>
2024-01-20 20:50:29 -0500

    finished styling header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,8 +2,8 @@
 function Header () {
     return (
         <header className="bg-primary flex justify-between p-2">
-            <section className="ml-2">Jenny Quan</section>
-            <ul className="flex">
+            <section className="ml-2 font-semibold text-lg self-center text-textColor">Jenny Quan</section>
+            <ul className="md:flex">
                 <li className="mr-2">About</li>
                 <li className="mr-2">Projects</li>
                 <li className="mr-2">Contact</li>
