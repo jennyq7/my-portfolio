d65b574a42042ddcb720631a58fbc0219eb78bf7
codingisfun <61808280+jennyq7@users.noreply.github.com>
2024-01-26 17:38:00 -0500

    worked on styling

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -2,14 +2,16 @@ import HeroIcon from '../../assets/girl-in-front-of-coding.jpg';
 
 function Hero () {
 
-    return (
-        <div className="hero bg-greyColor p-2 flex flex-col flex-wrap items-center">
-            <section>Frontend/Full Stack Developer</section>
-            <p className="self-center">I am a puzzle loving software developer who designs and develop solutions of benefit to others.</p>
-            <img src={HeroIcon} alt="girl in front of code"/>
-        </div>
 
+    return (
+        <div className="hero bg-greyColor p-2 flex flex-col pt-5">
+            <section className="mx-auto">Frontend/Full Stack Developer</section>
+            <p className="mx-auto pb-3">I am a puzzle loving software developer who designs and develop solutions of benefit to others.</p>
+            <img className="object-contain w-2/4 max-h-56 mx-auto" src={HeroIcon} alt="girl in front of code"/>
+        </div>
     )
 };
 
 export default Hero;
+
+
