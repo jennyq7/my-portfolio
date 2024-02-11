e39c47a31f286597ff9cf422ef0fe6f54e708eb0
codingisfun <61808280+jennyq7@users.noreply.github.com>
2024-02-11 17:28:05 -0500

    working on first project section

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,10 +1,23 @@
+import Calc from '../../assets/calc-app.png';
+import Unit from '../../assets/unit-converter-pic.png';
+import RecipeBox from '../../assets/recipe-short.png';
+
 function Projects ()  {
     return (
         <div className="projects p-2 pt-3 pl-3 bg-greyColor">
-            <h2 className="font-semibold">Projects</h2>
-            <div>
-            
-            </div>
+            <h2 className="font-semibold pb-2">Projects</h2>
+            <section>
+                <div>
+                   <img className="h-1/5 w-full" src={RecipeBox} alt="screenshot of recipe box main page"/>
+                   <div>
+                        <p>Recipe Box is my capstone with BrainStation and it is a full stack web app that allows you to sort and find recipes and save and add recipes with ease. 
+                        One notable feature is the ability to sort recipes by number of ingredients allowing users who do not have many ingredients at home to find a recipe with fewer ingredients
+                        so they do not have to buy items they cannot use up and would have expired otherwise. It uses HTML, CSS, SASS, JavaScript, Axios, React JS, Express JS, MySQL and Node JS and Knex JS. 
+                        The site is hosted on Azure Static Web App with auto-generated url. In the backend it uses Azure App Service and MySQL database.</p>                       
+                        <aside>Click <a href="https://brave-grass-0f08f400f.3.azurestaticapps.net/recipes">here</a> to go to my recipe site</aside> 
+                    </div>
+                </div>
+            </section>
         </div>
     )
 };
