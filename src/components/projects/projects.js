91412dff2d8f159a9796899b8baa6caff5ab7e2d
codingisfun <61808280+jennyq7@users.noreply.github.com>
2024-02-20 15:52:46 -0500

    fixed first 2 projects sections

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -7,9 +7,9 @@ function Projects ()  {
         <div className="projects p-2 pt-3 pl-3 bg-greyColor">
             <h2 className="font-semibold pb-4">Projects</h2>
             <section className="md:flex md:flex-wrap">
-                <div className="pb-3 md:flex md:gap-4">
-                   <div className="w-full">
-                      <img className="pb-2 mx-auto h-30v md:h-40v object-center object-cover" src={RecipeBox} alt="screenshot of recipe box main page"/>
+                <div className="pb-3 md:flex md:gap-2 xl:w-2/4">
+                   <div className="w-full md:w-80w h-auto">
+                      <img className="pb-2 mx-auto h-30v md:h-40v md:w-full object-center md:object-contain" src={RecipeBox} alt="screenshot of recipe box main page"/>
                    </div>
                    <div className="mx-auto md:self-center">
                         <p>Recipe Box is my capstone with BrainStation and it is a full stack web app that allows you to sort and find recipes and save and add recipes with ease. 
@@ -19,15 +19,24 @@ function Projects ()  {
                         <aside>Click <a className="text-blue-600" href="https://brave-grass-0f08f400f.3.azurestaticapps.net/recipes">here</a> to go to my recipe site</aside> 
                     </div>
                 </div>
-                <div className="pb-3 md:flex md:gap-4">
-                   <div className="w-full">
-                      <img className="pb-2 mx-auto h-30v md:h-40v object-center object-cover" src={Unit} alt="screenshot of unit converter"/>
+                <div className="md:flex md:gap-1 xl:w-2/4">
+                   <div className="w-full md:w-3/12 h-auto">
+                      <img className="pb-2 mx-auto md:m-0 h-30v md:h-40v md:w-full object-center md:object-contain" src={Unit} alt="screenshot of unit converter"/>
                    </div>
                    <div className="mx-auto md:self-center">
                         <p>Unit converter is a tool I created to help people convert from ounces to millimeters, 
                         inspired by local juice store. It uses React, Typescript, Javascript, HTML, CSS and SASS.</p>                       
                         <aside>Click <a className="text-blue-600" href="https://jenny-unit-converter.netlify.app/">here</a> to go to my unit site</aside> 
-                    </div>
+                   </div>
+                </div>
+                <div className="pb-3 md:flex md:gap-1 xl:w-2/4">
+                   <div className="w-full md:w-4/12 h-auto">
+                      <img className="mx-auto md:m-0 h-20v md:h-40v md:w-11/12 object-center md:object-contain" src={Calc} alt="screenshot of calculator"/>
+                   </div>
+                   <div className="mx-auto md:self-center">
+                        <p>Calculator-app is a calculator I created with React, JavaScript and TypeScript. It can perform the different operations, delete entries and showcase the result.</p>                                           
+                        <aside>Click <a className="text-blue-600" href="https://calculator-app-typescript-react.netlify.app/">here</a> to go to my calculator site</aside> 
+                   </div>
                 </div>
             </section>
         </div>
